82428ca5c28264dcb4979b058fa1c57cd668ff31
TheBlav <95691982+TheBlav@users.noreply.github.com>
2022-05-22 21:26:21 +0200

    Initial commit

---
+++ src/BigIntSum.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.math.BigInteger;
+
+public class BigIntSum {
+
+
+    public static void main(String[] args) {
+        Function function = new Function();
+        function.Function(123,1234,12345, BigInteger.valueOf(123456789), BigInteger.valueOf(987654321));
+    }
+
+}
---
+++ src/Function.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+
+public class Function {
+    public void Function(int a, int b, int c, BigInteger d, BigInteger e){
+        System.out.println(a+b+c);
+        System.out.println(d.add(e));
+    }
+}
